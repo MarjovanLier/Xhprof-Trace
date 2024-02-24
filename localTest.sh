1fc0efd48d3c8a431cf6a83830949e3fac985aa8
Marjo van Lier <marjo.vanlier@gmail.com>
2024-02-24 23:05:32 +0100

    Update Docker commands in localTest.sh
    Removed building of php-xprof-trace-composer-80 Docker image and its associated commands in localTest.sh. Also, Docker commands for PHP 8.1, 8.2, and 8.3 have been updated to mount a newly created directory for Docker's

--- localTest.sh
+++ localTest.sh
@@ -2,26 +2,19 @@
 
 set -euo pipefail
 
-docker build -t php-xprof-trace-composer-80 -f docker/Dockerfile80 .
+
 docker build -t php-xprof-trace-composer-81 -f docker/Dockerfile81 .
 docker build -t php-xprof-trace-composer-82 -f docker/Dockerfile82 .
 docker build -t php-xprof-trace-composer-83 -f docker/Dockerfile83 .
 
-DOCKER_CMD_80="docker run -it --rm -v $(pwd):/var/www php-xprof-trace-composer-80"
-DOCKER_CMD_81="docker run -it --rm -v $(pwd):/var/www php-xprof-trace-composer-81"
-DOCKER_CMD_82="docker run -it --rm -v $(pwd):/var/www php-xprof-trace-composer-82"
-DOCKER_CMD_83="docker run -it --rm -v $(pwd):/var/www php-xprof-trace-composer-83"
+mkdir -p "$HOME"/.composer/docker-cache
 
-### PHP 8.0
-rm -f composer.lock && \
-$DOCKER_CMD_80 composer --version && \
-$DOCKER_CMD_80 composer install && \
-$DOCKER_CMD_80 composer test:vulnerabilities-check && \
-$DOCKER_CMD_80 composer test:phpunit && \
+DOCKER_CMD_81="docker run -it --rm -v $(pwd):/var/www -v $HOME/.composer/docker-cache:/root/.composer php-xprof-trace-composer-81"
+DOCKER_CMD_82="docker run -it --rm -v $(pwd):/var/www -v $HOME/.composer/docker-cache:/root/.composer php-xprof-trace-composer-82"
+DOCKER_CMD_83="docker run -it --rm -v $(pwd):/var/www -v $HOME/.composer/docker-cache:/root/.composer php-xprof-trace-composer-83"
 
 ### PHP 8.1
 rm -f composer.lock && \
-$DOCKER_CMD_81 composer --version && \
 $DOCKER_CMD_81 composer install && \
 $DOCKER_CMD_81 composer test:vulnerabilities-check && \
 $DOCKER_CMD_81 composer test:phpunit && \
