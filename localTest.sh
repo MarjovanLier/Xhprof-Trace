90c189d353b390586f7a22229afb427dc96dd0e1
Marjo van Lier <marjo.vanlier@gmail.com>
2024-02-24 23:08:25 +0100

    Refactor local test script with loop
    The localTest.sh script has been refactored to reduce repetition. Instead of spelling out each test for different PHP versions, a loop is now used to handle multiple versions automatically. This increases the script's maintainability and eases the addition of further PHP versions in the future.

--- localTest.sh
+++ localTest.sh
@@ -9,30 +9,13 @@ docker build -t php-xprof-trace-composer-83 -f docker/Dockerfile83 .
 
 mkdir -p "$HOME"/.composer/docker-cache
 
-DOCKER_CMD_81="docker run -it --rm -v $(pwd):/var/www -v $HOME/.composer/docker-cache:/root/.composer php-xprof-trace-composer-81"
-DOCKER_CMD_82="docker run -it --rm -v $(pwd):/var/www -v $HOME/.composer/docker-cache:/root/.composer php-xprof-trace-composer-82"
-DOCKER_CMD_83="docker run -it --rm -v $(pwd):/var/www -v $HOME/.composer/docker-cache:/root/.composer php-xprof-trace-composer-83"
-
-### PHP 8.1
-rm -f composer.lock && \
-$DOCKER_CMD_81 composer install && \
-$DOCKER_CMD_81 composer test:vulnerabilities-check && \
-$DOCKER_CMD_81 composer test:phpunit && \
-
-
-### PHP 8.2
-rm -f composer.lock && \
-$DOCKER_CMD_82 composer --version && \
-$DOCKER_CMD_82 composer install && \
-$DOCKER_CMD_82 composer test:vulnerabilities-check && \
-$DOCKER_CMD_82 composer test:phpunit && \
-
-
-### PHP 8.3
-rm -f composer.lock && \
-$DOCKER_CMD_83 composer --version && \
-$DOCKER_CMD_83 composer install && \
-$DOCKER_CMD_83 composer test:vulnerabilities-check && \
-$DOCKER_CMD_83 composer test:phpunit && \
+for version in 80 81 82 83; do
+  DOCKER_CMD="docker run -it --rm -v $(pwd):/var/www -v $HOME/.composer/docker-cache:/root/.composer php-xprof-trace-composer-$version"
+  rm -f composer.lock && \
+  $DOCKER_CMD composer --version && \
+  $DOCKER_CMD composer install && \
+  $DOCKER_CMD composer test:vulnerabilities-check && \
+  $DOCKER_CMD composer test:phpunit
+done
 
 echo "All tests passed successfully!"
