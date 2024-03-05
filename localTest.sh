339ed7c7b7cda1fe2e195c21c4121aaed3cdb397
Marjo van Lier <marjo.vanlier@gmail.com>
2024-03-05 15:05:17 +0100

    Refactor local tests script for PHP versions
    The localTest.sh script has been improved for better handling of multiple PHP versions. Docker images are now built in a loop through PHP versions 81

--- localTest.sh
+++ localTest.sh
@@ -2,23 +2,44 @@
 
 set -euo pipefail
 
-
-docker build -t php-xprof-trace-composer-81 -f docker/Dockerfile81 .
-docker build -t php-xprof-trace-composer-82 -f docker/Dockerfile82 .
-docker build -t php-xprof-trace-composer-83 -f docker/Dockerfile83 .
-
 mkdir -p "$HOME"/.composer/docker-cache
 
-for version in 81 82 83; do
-  DOCKER_CMD="docker run -it --rm -v $(pwd):/var/www -v $HOME/.composer/docker-cache:/root/.composer php-xprof-trace-composer-$version"
-  rm -f composer.lock && \
-  $DOCKER_CMD composer --version && \
-  $DOCKER_CMD composer install && \
-  $DOCKER_CMD composer test:vulnerabilities-check && \
-  $DOCKER_CMD composer test:lint && \
-  $DOCKER_CMD composer test:code-style && \
-  $DOCKER_CMD composer test:phpmd && \
-  $DOCKER_CMD composer test:phpunit
+# Loop through PHP versions to build Docker images and run Composer commands
+for version in {81..83}; do
+  # Build Docker image for the current PHP version
+  IMAGE_NAME="php-xprof-trace-composer-$version"
+  docker build -t $IMAGE_NAME -f docker/Dockerfile"$version" .
+  if [ $? -ne 0 ]; then
+    echo "Docker build failed for PHP version $version"
+    exit 1
+  fi
+
+  # Define Docker run command
+  DOCKER_CMD="docker run -it --rm -v $(pwd):/var/www -v $HOME/.composer/docker-cache:/root/.composer $IMAGE_NAME"
+  $DOCKER_CMD composer --version
+  $DOCKER_CMD composer update && exit 1
+
+  # Remove existing composer.lock, display Composer version, and run Composer install and update for PHP 8.1
+  if [ "$version" -eq 81 ]; then
+    rm -f composer.lock && \
+    $DOCKER_CMD composer install && \
+    $DOCKER_CMD composer update --with-all-dependencies && \
+    $DOCKER_CMD composer require --dev --with-all-dependencies rector/rector
+
+    # Check for errors immediately after Composer commands
+    if [ $? -ne 0 ]; then
+        echo "Composer install failed for PHP $version"
+        exit 1
+    fi
+  fi
+
+  $DOCKER_CMD composer tests
+
+  # If any of the above commands fail, the script will stop and exit with a non-zero status code.
+  if [ $? -ne 0 ]; then
+    echo "Tests failed for PHP $version"
+    exit 1
+  fi
 done
 
 echo "All tests passed successfully!"
